9454e3ab17efcbeded8084cfd2ff0b24bd70a1da
sethuraman kannan <222756480+TensorStrike28-git@users.noreply.github.com>
2025-12-11 14:09:34 +0530

    Commit

--- RailGazer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Running FOIS RailGazer Automation...
-docker run --rm -v "%cd%":/app railgazer
-echo --------------------------------------------
-echo DONE. Excel file saved in this folder.
-pause
